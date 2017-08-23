5c6a8e4a196aa4958260e8a334a03b2fba41431d
xuzhe <geniuszhe@163.com>
2017-08-23 15:27:55 +0800

    add nginx load blancer

---
+++ package/scripts/nginx.py
@@ -0,0 +1,40 @@
+import os
+import base64
+from time import sleep
+from resource_management import *
+
+class NginxMaster(Script):
+    nginx_packages = ['nginx']
+    def install(self, env):      
+        import params
+        self.install_packages(env)
+        if self.nginx_packages is not None and len(self.nginx_packages):
+            for pack in self.nginx_packages:
+                Package(pack) 
+                
+
+    def configure(self, env):  
+        import params
+        env.set_params(params)
+        File(format("/etc/nginx/nginx.conf"), content=InlineTemplate(params.nginx_conf))
+             
+    def start(self, env):
+        import params
+        env.set_params(params)
+        self.configure(env)
+        Execute("service nginx start")
+        
+
+    def stop(self, env):
+        Execute("service nginx stop")
+
+
+    def restart(self, env):
+        Execute("service nginx restart")
+
+    def status(self, env):
+        Execute("service nginx status")
+
+
+if __name__ == "__main__":
+    NginxMaster().execute()
